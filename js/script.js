29a1f8ac9758ae47510bc5b069b01668d65a5176
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2023-12-20 17:57:03 +0100

    add DOM layout elements

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ console.log('JS OK');
 
 // CONSOLE OUTPUT SECTION:
 
-//Ciclo per conteggio da 1 a 100
+ //Ciclo per conteggio da 1 a 100
 for (let i = 1; i <= 100; i++) {
-    // Verifica del valore "i" multiplo di 3 e di 5: se "i modulo di 3" restituisce 0, quindi non ha resto, è multiplo di 3. altrettanto per il 5. se entrambe le condizioni sono verificate. stampo in console "FizzBuzz"
+    // Verifica del valore "i" multiplo di 3 e di 5: se "i modulo di 3" restituisce 0, quindi non ha resto, è multiplo di 3. altrettanto per il 5. se entrambe le condizioni sono verificate. stampa in console "FizzBuzz"
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
     }
@@ -21,4 +21,4 @@ for (let i = 1; i <= 100; i++) {
     else {
         console.log(i);
     }
-}
+} 
