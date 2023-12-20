5e4e8f420b16796b9fcbfd7731f7186ae7207325
Lorenzo7991 <lorenzo1997chierisini@gmail.com>
2023-12-20 18:53:15 +0100

    add colored squares by JS

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ console.log('JS OK');
 
 // CONSOLE OUTPUT SECTION:
 
- //Ciclo per conteggio da 1 a 100
+/*  //Ciclo per conteggio da 1 a 100
 for (let i = 1; i <= 100; i++) {
     // Verifica del valore "i" multiplo di 3 e di 5: se "i modulo di 3" restituisce 0, quindi non ha resto, è multiplo di 3. altrettanto per il 5. se entrambe le condizioni sono verificate. stampa in console "FizzBuzz"
     if (i % 3 === 0 && i % 5 === 0) {
@@ -22,3 +22,32 @@ for (let i = 1; i <= 100; i++) {
         console.log(i);
     }
 } 
+ */
+const rowElement = document.querySelector('.row');
+
+for (let i = 1; i <= 100; i++) {
+    let squareResult = "";
+    let bgColor = "";
+    
+    if (i % 3 === 0 && i % 5 === 0) {
+        squareResult = "BuzzFizz";
+        bgColor = "bg-red";
+    } else if (i % 3 === 0) {
+        squareResult = "Fizz";
+        bgColor = "bg-green";
+    } else if (i % 5 === 0) {
+        squareResult = "Buzz";
+        bgColor = "bg-yellow";
+    } else {
+        squareResult = i.toString();
+        bgColor = "bg-blue";
+    }
+
+    rowElement.innerHTML += `
+        <div class="col">
+            <div class="square d-flex justify-content-center align-items-center ${bgColor}">
+            <span class="square-text fw-bolder fs-6">${squareResult}</span>
+            </div>
+        </div>
+    `;
+}
